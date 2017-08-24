26d07d35ec1fcc4ca5f051a3caabbbeefa645091
Cuong, Duong <cuong.duong@halliburton.com>
2017-08-23 23:21:51 -0500

    Initial commit

---
+++ csvreader/csvreader.go
@@ -0,0 +1,185 @@
+/*
+http://www.apache.org/licenses/LICENSE-2.0.txt
+
+Copyright 2016 Intel Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package csvreader
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/intelsdi-x/snap-plugin-lib-go/v1/plugin"
+)
+
+const (
+	// Name of plugin
+	Name = "csvreader"
+	// Version of plugin
+	Version = 4
+)
+
+// CSVReader collector implementation used for testing
+type CSVReader struct {
+	configStr       map[string]string
+	configInt       map[string]int64
+	currentRowIndex int
+}
+
+// New logs collector plugin instance
+func New() *CSVReader {
+	return &CSVReader{}
+}
+
+// CollectMetrics collects metrics for testing
+func (c *CSVReader) CollectMetrics(mts []plugin.Metric) ([]plugin.Metric, error) {
+	if err := c.loadConfig(mts[0].Config); err != nil {
+		return nil, err
+	}
+	logrus.WithFields(logrus.Fields{"ColumnIndex": c.configStr["Index"], "file": c.configStr["file"]}).Info("CollectMetrics")
+	metrics := []plugin.Metric{}
+	var strVal string
+	var sliceStr []string
+	var headers []string
+
+	csvPath := c.configStr["file"]
+	if csvFile, err := os.Open(csvPath); err == nil {
+		defer csvFile.Close()
+		csvReader := csv.NewReader(csvFile)
+		headers, err = csvReader.Read()
+		pos := 1
+		for {
+			sliceStr, err = csvReader.Read()
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+			}
+			if c.currentRowIndex == pos {
+				break
+			}
+			pos++
+		}
+		c.currentRowIndex++
+
+		indexes := strings.Split(c.configStr["indexes"], ",")
+		units := strings.Split(c.configStr["units"], ",")
+		fileMetrics := []plugin.Metric{}
+		for i, colIdxStr := range indexes {
+			// Add new metric base on task configuration
+			ns := make([]plugin.NamespaceElement, len(mts[0].Namespace))
+			copy(ns, mts[0].Namespace)
+			colIdxStr = strings.TrimSpace(colIdxStr)
+			if colIdx, err := strconv.Atoi(colIdxStr); err == nil {
+				ns[2].Value = colIdxStr
+				mt := plugin.Metric{
+					Data:        0.1,
+					Unit:        units[i],
+					Description: headers[colIdx],
+					Timestamp:   time.Now(),
+					Version:     Version,
+					Namespace:   ns,
+				}
+				if strVal, err = c.getColumnFromCSV(sliceStr, colIdx); err == nil {
+					if mt.Data, err = strconv.ParseFloat(strVal, 64); err == nil {
+						fileMetrics = append(fileMetrics, mt)
+					}
+				}
+			}
+		}
+		// Return file metrics only if cache file successfully saved
+		metrics = append(metrics, fileMetrics...)
+	}
+	// Return file metrics only if cache file successfully saved
+	return metrics, nil
+}
+
+//GetMetricTypes returns metric types for testing
+func (c *CSVReader) GetMetricTypes(cfg plugin.Config) ([]plugin.Metric, error) {
+	if err := c.loadConfig(cfg); err != nil {
+		return nil, err
+	}
+
+	mts := []plugin.Metric{}
+	mts = append(mts, plugin.Metric{
+		Namespace:   plugin.NewNamespace("intel", Name).AddDynamicElement("Index", "Index of column").AddStaticElement("index"),
+		Description: "Single Column Index",
+		Unit:        "string",
+		Version:     Version,
+	})
+
+	return mts, nil
+}
+
+//GetConfigPolicy returns a ConfigPolicy for testing
+func (c *CSVReader) GetConfigPolicy() (plugin.ConfigPolicy, error) {
+	policy := plugin.NewConfigPolicy()
+	policy.AddNewStringRule([]string{"intel", Name}, "file", false, plugin.SetDefaultString("/opt/snap/files/metrics.csv"))
+	policy.AddNewStringRule([]string{"intel", Name}, "indexes", false, plugin.SetDefaultString("0,1"))
+	policy.AddNewStringRule([]string{"intel", Name}, "units", false, plugin.SetDefaultString("unit,unit"))
+	return *policy, nil
+}
+
+// Load config values
+func (c *CSVReader) loadConfig(cfg plugin.Config) error {
+	c.configStr = make(map[string]string)
+	c.configInt = make(map[string]int64)
+
+	for key := range cfg {
+		if val, err := cfg.GetInt(key); err == nil {
+			c.configInt[key] = val
+		}
+		if val, err := cfg.GetString(key); err == nil {
+			c.configStr[key] = val
+		}
+	}
+
+	return nil
+}
+
+func getSliceValueAtIndex(sliceStr []string, idx int) (string, error) {
+	for k, v := range sliceStr {
+		if k == idx {
+			return v, nil
+		}
+	}
+	return "", fmt.Errorf("Value not found at %d", idx)
+}
+
+func (c *CSVReader) getColumnFromCSV(sliceStr []string, colIdx int) (string, error) {
+	var strEmptyVal string = ""
+	for k, v := range sliceStr {
+		if k == colIdx {
+			return trimQuotes(v), nil
+		}
+	}
+	return trimQuotes(strEmptyVal), fmt.Errorf("Value not found at %d", colIdx)
+}
+
+func trimQuotes(s string) string {
+	if len(s) >= 2 {
+		if s[0] == '"' && s[len(s)-1] == '"' {
+			return s[1 : len(s)-1]
+		}
+	}
+	return s
+}
---
+++ csvreader/csvreader_test.go
@@ -0,0 +1,131 @@
+/*
+http://www.apache.org/licenses/LICENSE-2.0.txt
+
+
+Copyright 2016 Intel Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package csvreader
+
+import (
+	"testing"
+
+	"github.com/intelsdi-x/snap-plugin-lib-go/v1/plugin"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestLoadConfig(t *testing.T) {
+	cfg := make(plugin.Config)
+	cfg["file"] = "/var/cache/snap"
+	cfg["indexes"] = "0,1"
+	cfg["units"] = "max,min"
+
+	Convey("should not panic", t, func() {
+		So(func() {
+			l := CSVReader{}
+			l.loadConfig(cfg)
+		}, ShouldNotPanic)
+	})
+
+	Convey("should load config properly", t, func() {
+		l := CSVReader{}
+		l.loadConfig(cfg)
+
+		So(len(l.configInt), ShouldEqual, 0)
+		So(len(l.configStr), ShouldEqual, 3)
+	})
+}
+
+func TestGetMetricTypes(t *testing.T) {
+	cfg := make(plugin.Config)
+	cfg["file"] = "/opt/snap/files/metrics.csv"
+	cfg["indexes"] = "0,1,2"
+	cfg["units"] = "u1,u2,u3"
+
+	Convey("should not panic", t, func() {
+		So(func() {
+			l := CSVReader{}
+			l.GetMetricTypes(cfg)
+		}, ShouldNotPanic)
+	})
+
+	Convey("should return valid metric type", t, func() {
+		l := CSVReader{}
+		mt, err := l.GetMetricTypes(cfg)
+		So(err, ShouldBeNil)
+		So(mt, ShouldNotBeEmpty)
+		So(len(mt), ShouldEqual, 1)
+		So(mt[0].Namespace.Strings(), ShouldResemble, []string{"intel", "csvreader", "*", "index"})
+	})
+}
+
+func makeMetric(metricName string, cfg plugin.Config) []plugin.Metric {
+	mts := []plugin.Metric{
+		plugin.Metric{
+			Namespace: plugin.NewNamespace("intel", "csvreader").AddDynamicElement("Index", "Index name defined by indexes").AddStaticElement("index"),
+			Config:    cfg,
+		},
+	}
+	mts[0].Namespace[2].Value = metricName
+
+	return mts
+}
+
+func joinMetricData(m []plugin.Metric) string {
+	allData := ""
+	for _, v := range m {
+		allData += v.Data.(string)
+	}
+	return allData
+}
+
+func TestCollectMetrics(t *testing.T) {
+	Convey("should not panic and return valid namespace", t, func() {
+		cfgIndex01 := make(plugin.Config)
+		cfgIndex01["file"] = "/opt/snap/files/metrics.csv"
+		cfgIndex01["indexes"] = "0,1"
+		cfgIndex01["units"] = "date,psi"
+		mtsIndex0 := makeMetric("0", cfgIndex01)
+		l := CSVReader{}
+		mts, err := l.CollectMetrics(mtsIndex0)
+		So(err, ShouldBeNil)
+		for _, m := range mts {
+			ok, dyn := m.Namespace.IsDynamic()
+			So(ok, ShouldBeTrue)
+			So(dyn, ShouldResemble, []int{2, 3})
+		}
+	})
+}
+
+func TestGetConfigPolicy(t *testing.T) {
+	Convey("should not panic", t, func() {
+		So(func() {
+			l := CSVReader{}
+			l.GetConfigPolicy()
+		}, ShouldNotPanic)
+	})
+
+	Convey("should resemble default config policy", t, func() {
+		defaultPolicy := plugin.NewConfigPolicy()
+		defaultPolicy.AddNewStringRule([]string{"intel", Name}, "file", false, plugin.SetDefaultString("/opt/snap/files/metrics.csv"))
+		defaultPolicy.AddNewStringRule([]string{"intel", Name}, "indexes", false, plugin.SetDefaultString("0,1"))
+		defaultPolicy.AddNewStringRule([]string{"intel", Name}, "units", false, plugin.SetDefaultString("min,max"))
+
+		l := CSVReader{}
+		policy, err := l.GetConfigPolicy()
+		So(err, ShouldBeNil)
+		So(policy, ShouldResemble, *defaultPolicy)
+	})
+}
